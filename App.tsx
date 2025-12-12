f23efb1ddbd9931ba01f51c7b6def58073d7d474
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-12-12 11:28:32 +0700

    firs commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello App Using native device</Text>
+    </View>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome to your Expo app 👋 - DEV With Thien Bao 97 
+
+
