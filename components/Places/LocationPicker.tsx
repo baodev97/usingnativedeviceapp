cfe4f3ca757e7e639724543b25e679b16bcdb333
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-12-14 18:17:24 +0700

    Locating Users

--- components/Places/LocationPicker.tsx
+++ components/Places/LocationPicker.tsx
@@ -1,28 +1,69 @@
 import { Colors } from "@/constants/colors";
-import { StyleSheet, View } from "react-native";
+import {
+    getCurrentPositionAsync,
+    PermissionStatus,
+    useForegroundPermissions,
+} from "expo-location";
+import { Alert, StyleSheet, View } from "react-native";
 import OutlineButton from "../UI/OutlineButton";
 
 function LocationPicker() {
+  const [locationPermissionInfomation, requestPermission] =
+    useForegroundPermissions();
+
+  async function verifyPermissions() {
+    if (
+      locationPermissionInfomation?.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+    if (locationPermissionInfomation?.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions",
+        "you need to grant location permissions to use this app"
+      );
+      return false;
+    }
+    return true;
+  }
+
+  async function getLocationHandler() {
+    try {
+      const hasPermission = await verifyPermissions();
+      if (!hasPermission) {
+        return
+      }
+      let location = await getCurrentPositionAsync();
+        console.log(location);
+    } catch (error) {
+      console.log(error);
+    }
+
+    return;
+  }
+  function pickOnMapHandler() {}
+
   return (
     <View>
       <View style={styles.mapPreview}></View>
-        <View style={styles.actions}>
-          <OutlineButton
-            name="location"
-            color={Colors.primary500}
-            onPress={() => {}}
-          >
-            Locate User
-          </OutlineButton>
-          <OutlineButton
-            name="map"
-            color={Colors.primary500}
-            onPress={() => {}}
-          >
-            Pick on Map
-          </OutlineButton>
-        </View>
+      <View style={styles.actions}>
+        <OutlineButton
+          name="location"
+          color={Colors.primary500}
+          onPress={getLocationHandler}
+        >
+          Locate User
+        </OutlineButton>
+        <OutlineButton
+          name="map"
+          color={Colors.primary500}
+          onPress={pickOnMapHandler}
+        >
+          Pick on Map
+        </OutlineButton>
       </View>
+    </View>
   );
 }
 
