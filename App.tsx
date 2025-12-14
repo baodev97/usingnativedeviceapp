36c3ae48a99ab8d558dd2a421309e51b5ddeee89
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-12-14 09:49:26 +0700

    Adding a Header Button

--- App.tsx
+++ App.tsx
@@ -1,28 +1,39 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar, StyleSheet } from "react-native";
+import IconButton from "./components/UI/IconButton";
 import AddPlace from "./screen/AddPlace";
 import AllPlaces from "./screen/AllPlaces";
 
 const Stack = createNativeStackNavigator();
 
-
 export default function App() {
   return (
     <>
-     <StatusBar barStyle={'dark-content'} />
+      <StatusBar barStyle={"dark-content"} />
 
-    <NavigationContainer>
+      <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="AllPlaces" component={AllPlaces}/>
-          <Stack.Screen name="AddPlace" component={AddPlace}/>
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  color={tintColor}
+                  size={24}
+                  name="add"
+                  onPress={() => navigation.navigate("AddPlace")}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen name="AddPlace" component={AddPlace} />
         </Stack.Navigator>
-    </NavigationContainer>
+      </NavigationContainer>
     </>
   );
 }
 const styles = StyleSheet.create({
-  appContainer:{
-
-  }
+  appContainer: {},
 });
