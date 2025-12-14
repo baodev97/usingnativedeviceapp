20d079d0463c163f255b413449ce904fcc4fb6b2
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-12-14 15:37:34 +0700

    showing a image preview

--- components/Places/ImagePicker.tsx
+++ components/Places/ImagePicker.tsx
@@ -1,13 +1,16 @@
+import { Colors } from "@/constants/colors";
 import {
     launchCameraAsync,
     PermissionStatus,
     useCameraPermissions,
 } from "expo-image-picker";
-import { Alert, Button, View } from "react-native";
+import { useState } from "react";
+import { Alert, Button, Image, StyleSheet, Text, View } from "react-native";
 
 function ImagePicker() {
   const [cameraPermissionInformation, requestPermission] =
     useCameraPermissions();
+  const [pickedImage, setPickedImage] = useState<undefined | string>();
 
   async function verifyPermissions() {
     if (cameraPermissionInformation?.status === PermissionStatus.UNDETERMINED) {
@@ -25,21 +28,50 @@ function ImagePicker() {
   }
 
   async function takImageHandler() {
-    const hasPermission = verifyPermissions();
+    const hasPermission = await verifyPermissions();
     if (!hasPermission) return;
-    const image = await launchCameraAsync({
+    const result = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
       quality: 0.5,
     });
-    console.log(image);
-  }
+    if (result.canceled) {
+      return;
+    }
+    const imageUri = result.assets?.[0]?.uri;
 
+    if (imageUri) {
+      setPickedImage(imageUri);
+    }
+  }
+  let imagePreview = <Text>No Image taken yet.</Text>;
+  if(pickedImage){
+    imagePreview = <Image style={styles.image} source={{uri:pickedImage}}/>
+  }
   return (
     <View>
-      <View></View>
+      <View style={styles.imagePreview}>
+        {imagePreview}
+      </View>
       <Button title="Take Image" onPress={takImageHandler} />
     </View>
   );
 }
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+    imagePreview:{
+        width:'100%',
+        height:200,
+        marginVertical:8,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:Colors.primary100,
+        borderRadius:8
+    },
+    image:{
+        width:'100%',
+        height:'100%',
+        borderRadius:8
+    }
+})
