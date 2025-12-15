cc69bf88d230594917f9998a5e1f0fb88a20cac1
Le Cong Thien Bao <thienbao@Les-MacBook-Air.local>
2025-12-15 18:43:07 +0700

    create Map Screen with mapview

--- screen/Map.tsx
+++ screen/Map.tsx
@@ -1,10 +1,12 @@
-import { Text, View } from "react-native";
+import MapView, { Region } from "react-native-maps";
 
-function Map(){
-    return (
-        <View>
-             <Text>Mapp screen</Text>   
-        </View>
-    )
+function Map() {
+  const region: Region = {
+    latitude: 10.7221761,
+    longitude: 106.6587894,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+  return <MapView initialRegion={region}></MapView>;
 }
 export default Map;
